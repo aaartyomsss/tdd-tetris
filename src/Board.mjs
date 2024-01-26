0d0ec222d6ef17be876e8d8decf33a1cef5e2c85
aaartyomsss <aaartyomsss@gmail.com>
2024-01-26 20:42:15 +0200

    Stops when hits the bottom

--- src/Board.mjs
+++ src/Board.mjs
@@ -30,6 +30,10 @@ export class Board {
     for (let i = 0; i < this.height; i++) {
       const col = this.boardMatrix[i].indexOf(this.fallingElement)
       if (col === -1) continue
+      if (i + 1 === this.height) {
+        this.fallingElement = undefined
+        break
+      }
       this.boardMatrix[i][col] = '.'
       this.boardMatrix[i + 1][col] = this.fallingElement
       break
