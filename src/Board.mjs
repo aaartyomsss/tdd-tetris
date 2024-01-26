825f2a9108241dbbcb9b52608436256979934c77
aaartyomsss <aaartyomsss@gmail.com>
2024-01-26 20:17:45 +0200

    Refactor Board to use matrix as primary structure

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,16 +1,22 @@
 export class Board {
   width;
   height;
+  boardMatrix = [];
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    for (let i = 0; i < this.height; i++) {
+      const row = '.'.repeat(width).split('')
+      this.boardMatrix.push(row)
+    }
   }
 
   toString() {
     let result = ''
-    for (let i = 0; i < this.height; i++){
-      const row = `.`.repeat(3).concat('\n')
+    for (let i = 0; i < this.boardMatrix.length; i++){
+      const matrixRow = this.boardMatrix[i]
+      const row = matrixRow.join('').concat('\n')
       result += row
     }
     return result
