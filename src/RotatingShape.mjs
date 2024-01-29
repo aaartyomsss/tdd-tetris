cf6523f69bf08e0c102fec5857c8a5ffed335736
aaartyomsss <aaartyomsss@gmail.com>
2024-01-29 20:41:05 +0200

    Make rotateLeft immutable

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -26,13 +26,14 @@ export class RotatingShape {
     }
     
     rotateLeft() {
+        let a = this.shapeMatrix
         for (let i = 0; i < parseInt(this.height / 2); i++) {
             for (let j = i; j < this.height - i - 1; j++) {
-                const temp = this.shapeMatrix[i][j]
-                this.shapeMatrix[i][j] = this.shapeMatrix[j][this.height - 1 - i];
-                this.shapeMatrix[j][this.height - 1 - i] = this.shapeMatrix[this.height - 1 - i][this.height - 1 - j];
-                this.shapeMatrix[this.height - 1 - i][this.height - 1 - j] = this.shapeMatrix[this.height - 1 - j][i];
-                this.shapeMatrix[this.height - 1 - j][i] = temp;
+                const temp = a[i][j]
+                a[i][j] = a[j][this.height - 1 - i];
+                a[j][this.height - 1 - i] = a[this.height - 1 - i][this.height - 1 - j];
+                a[this.height - 1 - i][this.height - 1 - j] = a[this.height - 1 - j][i];
+                a[this.height - 1 - j][i] = temp;
             }
         }
         return this
