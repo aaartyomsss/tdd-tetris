9eef93e282542a8d57c7a24c059b9d5681c3d074
aaartyomsss <aaartyomsss@gmail.com>
2024-02-03 15:54:38 +0200

    Add rotateRight method

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -15,7 +15,15 @@ export class Tetromino {
         this.width = rows[0].length
         this.shapeMatrix = rows.map(row => row.split(""));
     }
-
+    rotateRight() {
+        let a = JSON.parse(JSON.stringify(this.shapeMatrix))
+        for (let i = 0; i < parseInt(this.height / 2); i++) {
+            for (let j = i; j < this.height - i - 1; j++) {const temp = a[i][j] 
+                a[i][j] = a[this.height - 1 - j][i];
+                a[this.height - 1 - j][i] = a[this.height - 1 - i][this.height - 1 - j];
+                a[this.height - 1 - i][this.height - 1 - j] = a[j][this.height - 1 - i];
+                a[j][this.height - 1 - i] = temp;}}
+        return new Tetromino(a.map(row => row.join('')).join('\n'))}
     toString() {
         let result = ''
         for (let i = 0; i < this.shapeMatrix.length; i++){
