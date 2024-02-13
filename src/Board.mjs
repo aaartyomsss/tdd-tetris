244cc83165d91f4d3eef72e179dc29bcb0e5533b
aaartyomsss <aaartyomsss@gmail.com>
2024-02-13 20:32:05 +0200

    Falling tetros on top of each other (Big diff due to formatting)

--- src/Board.mjs
+++ src/Board.mjs
@@ -3,111 +3,124 @@ export class Board {
   height;
   boardMatrix = [];
   fallingElement;
-  fallingElementTopLeftIndex
+  fallingElementTopLeftIndex;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     for (let i = 0; i < this.height; i++) {
-      const row = '.'.repeat(width).split('')
-      this.boardMatrix.push(row)
+      const row = ".".repeat(width).split("");
+      this.boardMatrix.push(row);
     }
   }
 
   hasFalling() {
-    return !!this.fallingElement 
+    return !!this.fallingElement;
   }
 
   drop(element) {
     if (this.fallingElement) {
-      throw new Error("already falling")
+      throw new Error("already falling");
     }
-    this.fallingElement = element
+    this.fallingElement = element;
     if (element.width && element.height) {
-      const startingPosition = Math.floor((this.width - element.width) / 2)
-      this.fallingElementTopLeftIndex = [startingPosition, 0]
+      const startingPosition = Math.floor((this.width - element.width) / 2);
+      this.fallingElementTopLeftIndex = [startingPosition, 0];
       for (let i = 0; i < element.height; i++) {
         for (let j = 0; j < element.width; j++) {
-          this.boardMatrix[i][startingPosition + j] = element.shapeMatrix[i][j]
+          this.boardMatrix[i][startingPosition + j] = element.shapeMatrix[i][j];
         }
       }
     } else {
-      const middleColumn = Math.floor(this.width / 2)
-      this.fallingElementTopLeftIndex = [middleColumn, 0]
-      this.boardMatrix[0][middleColumn] = element}
+      const middleColumn = Math.floor(this.width / 2);
+      this.fallingElementTopLeftIndex = [middleColumn, 0];
+      this.boardMatrix[0][middleColumn] = element;
+    }
   }
 
   rowIsCompletelyFree(row) {
     for (const i in row) {
-      if (row[i] !== '.') {
-        return false
+      if (row[i] !== ".") {
+        return false;
       }
     }
-    return true
+    return true;
   }
 
   isWidthFree(row, start, length) {
     for (let i = start; i < start + length; i++) {
-      if (row[i] !== '.') return false
+      if (row[i] !== ".") return false;
     }
-    return true
+    return true;
   }
 
   #moveTetromino(row, col) {
     for (let i = this.fallingElement.height - 1; i >= 0; i--) {
+      const isUpcomingRowCompletelyFree =
+        this.boardMatrix[row + i + 1] &&
+        this.isWidthFree(this.boardMatrix[row + i + 1], col, this.fallingElement.width);
       for (let j = this.fallingElement.width - 1; j >= 0; j--) {
-        if (row + i + 1 < this.height && '.' === this.boardMatrix[row + i + 1][col + j]) {
-          this.boardMatrix[row + i + 1][col + j] = this.fallingElement.shapeMatrix[i][j]
-        } 
-        if (row + i < this.height) {
-          this.boardMatrix[row + i][col + j] = '.'
+        if (row + i + 1 < this.height && "." === this.boardMatrix[row + i + 1][col + j]) {
+          this.boardMatrix[row + i + 1][col + j] = this.fallingElement.shapeMatrix[i][j];
         }
-      } 
-      console.log(this.toString())
-      if (!this.isWidthFree(this.boardMatrix[this.height - 1], col, this.fallingElement.width) && i === 0
-          || this.boardMatrix[col+this.fallingElement.height] && !this.isWidthFree(this.boardMatrix[col + this.fallingElement.height], col, this.fallingElement.width)) {
-        console.log("Does this cause the break?")
-        return true
+        if (row + i < this.height && isUpcomingRowCompletelyFree) {
+          this.boardMatrix[row + i][col + j] = ".";
+        }
+      }
+      if (
+        this.boardMatrix[col + this.fallingElement.height] &&
+        !this.isWidthFree(this.boardMatrix[col + this.fallingElement.height], col, this.fallingElement.width)
+      ) {
+        return true;
       }
     }
   }
 
   moveTetromino() {
-    const [col, row] = this.fallingElementTopLeftIndex
-    if (row + this.fallingElement.height === this.height 
-      && !this.rowIsCompletelyFree(this.boardMatrix[this.height - 1])) {
-        this.fallingElement = undefined
-        this.fallingElementTopLeftIndex = undefined
-        return
-      }
-    if (this.#moveTetromino(row, col)) return
-    this.fallingElementTopLeftIndex = [col, row + 1]
+    const [col, row] = this.fallingElementTopLeftIndex;
+    const isUpcomingSpaceInFrontOfTetroIsFree =
+      this.boardMatrix[row + this.fallingElement.height - this.fallingElement.freeRowsFromBottom()] &&
+      this.isWidthFree(
+        this.boardMatrix[row + this.fallingElement.height - this.fallingElement.freeRowsFromBottom()],
+        col,
+        this.fallingElement.width
+      );
+    if (
+      row + this.fallingElement.height === this.height &&
+      !this.rowIsCompletelyFree(this.boardMatrix[this.height - 1]) || !isUpcomingSpaceInFrontOfTetroIsFree
+    ) {
+      this.fallingElement = undefined;
+      this.fallingElementTopLeftIndex = undefined;
+      return;
+    }
+    if (this.#moveTetromino(row, col)) return;
+    this.fallingElementTopLeftIndex = [col, row + 1];
   }
 
   tick() {
-      if (!this.fallingElementTopLeftIndex) return
-      const [col, row] = this.fallingElementTopLeftIndex
-      if (row + 1 === this.height || (this.boardMatrix[row + 1][col] !== '.' && !this.fallingElement.width)) {
-        this.fallingElement = undefined
-        this.fallingElementTopLeftIndex = undefined
-        return
-      } if (!this.fallingElement.width) {
-        this.boardMatrix[row][col] = '.'
-        this.boardMatrix[row + 1][col] = this.fallingElement
-        this.fallingElementTopLeftIndex = [col, row + 1]
-      } else {
-        this.moveTetromino()
-      }
+    if (!this.fallingElementTopLeftIndex) return;
+    const [col, row] = this.fallingElementTopLeftIndex;
+    if (row + 1 === this.height || (this.boardMatrix[row + 1][col] !== "." && !this.fallingElement.width)) {
+      this.fallingElement = undefined;
+      this.fallingElementTopLeftIndex = undefined;
+      return;
+    }
+    if (!this.fallingElement.width) {
+      this.boardMatrix[row][col] = ".";
+      this.boardMatrix[row + 1][col] = this.fallingElement;
+      this.fallingElementTopLeftIndex = [col, row + 1];
+    } else {
+      this.moveTetromino();
+    }
   }
 
   toString() {
-    let result = ''
-    for (let i = 0; i < this.boardMatrix.length; i++){
-      const matrixRow = this.boardMatrix[i]
-      const row = matrixRow.join('').concat('\n')
-      result += row
+    let result = "";
+    for (let i = 0; i < this.boardMatrix.length; i++) {
+      const matrixRow = this.boardMatrix[i];
+      const row = matrixRow.join("").concat("\n");
+      result += row;
     }
-    console.log(result)
-    return result
+    return result;
   }
 }
