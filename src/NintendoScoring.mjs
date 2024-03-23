043eb8be2b5a76a5a369a59f5a72ec3d0a458b39
aaartyomsss <aaartyomsss@gmail.com>
2024-03-23 15:22:58 +0200

    Scoring system accepts level as a parameter

--- src/NintendoScoring.mjs
+++ src/NintendoScoring.mjs
@@ -2,4 +2,9 @@
 
 export class NintendoScoring {
     score = 0
+    level = 0
+
+    constructor(level = 0) {
+        this.level = level
+    }
 }
--- test/Scoring.test.mjs
+++ test/Scoring.test.mjs
@@ -7,6 +7,13 @@ describe("NintendoScoring", () => {
         const scoringSystem = new NintendoScoring()
 
         expect(scoringSystem.score).toEqual(0)
+        expect(scoringSystem.level).toEqual(0)
+    })
+
+    test("NintendoScoring accepts level as a parameter", () => {
+        const scoringSystem = new NintendoScoring(3)
+        expect(scoringSystem.score).toEqual(0)
+        expect(scoringSystem.level).toEqual(3)
     })
 
 })
