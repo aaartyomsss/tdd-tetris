813d753a110b0d96389b154494a18240efdea650
aaartyomsss <aaartyomsss@gmail.com>
2024-03-24 13:55:38 +0200

    UpdateScore on 1 row cleared

--- src/NintendoScoring.mjs
+++ src/NintendoScoring.mjs
@@ -7,4 +7,10 @@ export class NintendoScoring {
     constructor(level = 0) {
         this.level = level
     }
+
+    updateScore(rowsCleared) {
+        if (rowsCleared === 1) {
+            this.score += 40
+        }
+    }
 }
--- test/Scoring.test.mjs
+++ test/Scoring.test.mjs
@@ -16,4 +16,11 @@ describe("NintendoScoring", () => {
         expect(scoringSystem.level).toEqual(3)
     })
 
+    test("NintendoScoring is capable of performing and update on 1 row removed", () => {
+        const scoringSystem = new NintendoScoring()
+        expect(scoringSystem.score).toEqual(0)
+        scoringSystem.updateScore(1)
+        expect(scoringSystem.score).toEqual(40)
+    })
+
 })
